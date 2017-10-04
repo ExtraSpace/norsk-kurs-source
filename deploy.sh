488eb7ee1cbd4411fe8cd3415bc086363fe2fe1c
Raju Rimal <therimalaya@gmail.com>
2017-10-04 21:28:33 +0200

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-Rscript -e "blogdown::build_site()" # if using a theme, replace with `hugo -t <YOURTHEME>`
+Rscript -e "blogdown::build_site()" 
 
 # Go To Public folder
 cd public
